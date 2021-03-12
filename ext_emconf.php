07ab26e2ef2f323951aa85ddd0f9d6aab333f17b
Benjamin Serfhos <benjamin@serfhos.com>
2021-03-12 13:10:01 +0100

    [TASK] Update dependencies

--- ext_emconf.php
+++ ext_emconf.php
@@ -8,14 +8,14 @@ $EM_CONF[$_EXTKEY] = [
     'title' => 'Bynder integration for TYPO3',
     'description' => 'Integrate the Bynder DAM into TYPO3',
     'category' => 'distribution',
-    'version' => '0.0.3',
+    'version' => '0.0.3-dev',
     'state' => 'beta',
     'clearcacheonload' => 1,
     'author' => 'Frans Saris - Beech.it',
     'author_email' => 't3ext@beech.it',
     'constraints' => [
         'depends' => [
-            'typo3' => '>= 8.7.13 || >=9.5.20'
+            'typo3' => '>= 8.7.13 || >=9.5.20 || >= 10.4.13'
         ],
         'conflicts' => [],
         'suggests' => [],
